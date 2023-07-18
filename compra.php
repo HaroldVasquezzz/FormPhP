9b985568e4eb1568793b7621da54f363e2b005e9
HaroldVasquezzz <128524116+HaroldVasquezzz@users.noreply.github.com>
2023-07-18 16:59:07 -0500

    Create compra.php

---
+++ compra.php
@@ -0,0 +1,15 @@
+<?php
+  $producto1 = $_POST['producto1'];
+  $precio1 = $_POST['precio1'];
+  $producto2 = $_POST['producto2'];
+  $precio2 = $_POST['precio2'];
+  $total = $precio1 + $precio2;
+echo "Producto1:".$producto1;
+echo"<br>";
+  echo "Precio1:".$precio1;
+echo"<br>";
+  echo "Producto2:".$producto2;
+echo"<br>";
+  echo "Precio2:".$precio2;
+echo"<br>";
+  echo "Total a pagar:".$total;
